d01aa0e09ac0b6b28420ecddede03b1cc2092509
Vaibhavdimri78 <111555559+Vaibhavdimri78@users.noreply.github.com>
2022-08-27 04:02:25 -0700

    Add files via upload

---
+++ Assignment8.py
@@ -0,0 +1,21 @@
+n = int(input("enter the number of natural number want to print"))
+"""i=1
+while(i<=n):
+    print(i,end='')
+    i+=1
+a= n
+while(a>0):
+    print(a,end=' ')
+    a-=1
+i=1    
+while(i<=n):
+    print(i,end=' ')
+    i+=2
+    """
+i=1
+while(i<=n):
+    #print(i**i,end=' ') #this will print 1,2^2,3^3,4^4 and so on
+    #print(i**3,end=' ')
+    print(i*5,end=' ')
+    i+=1
+
